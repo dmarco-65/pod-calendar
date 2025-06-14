d05c754fa9e75266de0c22b5e28d9a7583ce43b7
Zoltounet <zolt65@gmail.com>
2025-06-14 16:39:16 +0200

    init projet spring

---
+++ src/main/java/pod/jdr/jdrpod/JdrPodApplication.java
@@ -0,0 +1,13 @@
+package pod.jdr.jdrpod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdrPodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JdrPodApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pod/jdr/jdrpod/JdrPodApplicationTests.java
@@ -0,0 +1,13 @@
+package pod.jdr.jdrpod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdrPodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
